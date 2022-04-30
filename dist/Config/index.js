eb9fb9f7ab48b2d48182fcac6e99278c8fbae522
Elvis Kemevor <kemevoralwise@gmail.com>
2022-04-30 14:54:55 +0000

    upload

--- dist/Config/index.js
+++ dist/Config/index.js
@@ -5,36 +5,36 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
-const dialect = 'postgres';
+const dialect = "postgres";
 exports.default = {
     database: {
-        uri: process.env.db_url || '',
+        uri: process.env.db_url || "",
         options: {
             dialect,
             dialectOptions: {
-                decimalNumbers: true
+                decimalNumbers: true,
                 // ssl: {
                 //     require: false, // This will help you. But you will see nwe error
                 //     rejectUnauthorized: false // This line will fix new error
                 //   }
-            }
-        }
+            },
+        },
     },
     keys: {
         PORT: process.env.PORT || 2500,
-        tokenSecrete: process.env.token_secret
+        tokenSecrete: process.env.token_secret,
     },
     fileExtensions: {
-        jpg: '.jpg'
+        jpg: ".jpg",
     },
     s3: {
         baseUrl: process.env.s3Url,
         options: {
             accessKeyId: process.env.accessKeyId,
             secretAccessKey: process.env.secretAccessKey,
-            ACL: 'public-read',
-            baseBucket: 'team',
-            region: 'eu-west-1'
+            ACL: "public-read",
+            baseBucket: "team",
+            region: "eu-west-1",
         },
     },
     email: {
@@ -44,7 +44,11 @@ exports.default = {
         senderName: process.env.senderName,
     },
     frontEnd: {
-        passwordResetLink: process.env.NODE_ENV == 'dev' ? 'http://localhost:3000/reset-password/?id=' : 'https://team.bookgmt.com/reset-password/?id=',
-        inviteLink: process.env.NODE_ENV == 'dev' ? 'http://localhost:3000/accept-invite/?id=' : 'https://team.bookgmt.com/accept-invite/?id=',
-    }
+        passwordResetLink: process.env.NODE_ENV == "dev"
+            ? "http://localhost:3000/reset-password/?id="
+            : "https://teams.bookgmt.com/reset-password/?id=",
+        inviteLink: process.env.NODE_ENV == "dev"
+            ? "http://localhost:3000/accept-invite/?id="
+            : "https://teams.bookgmt.com/accept-invite/?id=",
+    },
 };
--- src/Config/index.ts
+++ src/Config/index.ts
@@ -1,53 +1,56 @@
-import dotenv from 'dotenv';
-import { Dialect } from 'sequelize';
-dotenv.config()
-const dialect : Dialect = 'postgres';
+import dotenv from "dotenv";
+import { Dialect } from "sequelize";
+dotenv.config();
+const dialect: Dialect = "postgres";
 
 export default {
-    database:{
-        uri:process.env.db_url || '',
-        options: {
-            dialect,
-            dialectOptions:{
-                decimalNumbers: true
-                // ssl: {
-                //     require: false, // This will help you. But you will see nwe error
-                //     rejectUnauthorized: false // This line will fix new error
-                //   }
-            }
-        }
+  database: {
+    uri: process.env.db_url || "",
+    options: {
+      dialect,
+      dialectOptions: {
+        decimalNumbers: true,
+        // ssl: {
+        //     require: false, // This will help you. But you will see nwe error
+        //     rejectUnauthorized: false // This line will fix new error
+        //   }
+      },
     },
-    keys:{
-        PORT:process.env.PORT || 2500,
-        tokenSecrete:process.env.token_secret
+  },
+  keys: {
+    PORT: process.env.PORT || 2500,
+    tokenSecrete: process.env.token_secret,
+  },
+
+  fileExtensions: {
+    jpg: ".jpg",
+  },
+
+  s3: {
+    baseUrl: process.env.s3Url,
+    options: {
+      accessKeyId: process.env.accessKeyId,
+      secretAccessKey: process.env.secretAccessKey,
+      ACL: "public-read",
+      baseBucket: "team",
+      region: "eu-west-1",
     },
+  },
+  email: {
+    privateKey: process.env.privateKey,
+    publicKey: process.env.publicKey,
+    senderMail: process.env.senderMail,
+    senderName: process.env.senderName,
+  },
+  frontEnd: {
+    passwordResetLink:
+      process.env.NODE_ENV == "dev"
+        ? "http://localhost:3000/reset-password/?id="
+        : "https://teams.bookgmt.com/reset-password/?id=",
 
-    fileExtensions:{
-        jpg : '.jpg'
-    },
-   
-    s3 : {
-        baseUrl:process.env.s3Url,
-        options:{
-            accessKeyId:process.env.accessKeyId,
-            secretAccessKey:process.env.secretAccessKey,
-            ACL: 'public-read',
-            baseBucket:'team',
-            region:'eu-west-1'
-        },
-
-    },
-    email:{
-        privateKey: process.env.privateKey,
-        publicKey: process.env.publicKey,
-        senderMail: process.env.senderMail,
-        senderName: process.env.senderName,
-    },
-    frontEnd:{
-        passwordResetLink:process.env.NODE_ENV == 'dev' ? 'http://localhost:3000/reset-password/?id=' : 'https://team.bookgmt.com/reset-password/?id=',
-
-        inviteLink:process.env.NODE_ENV == 'dev' ? 'http://localhost:3000/accept-invite/?id=' : 'https://team.bookgmt.com/accept-invite/?id=',
-    }
-
-
-}
+    inviteLink:
+      process.env.NODE_ENV == "dev"
+        ? "http://localhost:3000/accept-invite/?id="
+        : "https://teams.bookgmt.com/accept-invite/?id=",
+  },
+};
